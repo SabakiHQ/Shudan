8daf78aa4617245cc0d6529cdf4344b50ab35b3b
Yichuan Shen <yichuan.shen@sap.com>
2018-06-28 14:55:36 +0200

    Fix helper.random()

--- src/helper.js
+++ src/helper.js
@@ -1,7 +1,7 @@
 exports.alpha = 'ABCDEFGHJKLMNOPQRSTUVWXYZ'
 
 exports.range = n => [...Array(n)].map((_, i) => i)
-exports.random = n => Math.floor(Math.random() * n)
+exports.random = n => Math.floor(Math.random() * (n + 1))
 exports.neighborhood = ([x, y]) => [[x, y], [x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]]
 exports.vertexEquals = ([x1, y1], [x2, y2]) => x1 === x2 && y1 === y2
 exports.lineEquals = ([v1, w1], [v2, w2]) => exports.vertexEquals(v1, v2) && exports.vertexEquals(w1, w2)
@@ -12,7 +12,7 @@ exports.getHoshis = function(width, height) {
     let [nearX, nearY] = [width, height].map(x => x >= 13 ? 3 : 2)
     let [farX, farY] = [width - nearX - 1, height - nearY - 1]
     let [middleX, middleY] = [width, height].map(x => (x - 1) / 2)
-    
+
     let result = [[nearX, farY], [farX, nearY], [farX, farY], [nearX, nearY]]
 
     if (width % 2 !== 0 && height % 2 !== 0)
