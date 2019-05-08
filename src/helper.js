3813269b937543d750d0b40f36a277a50dacadd8
Yichuan Shen <shen.yichuan@gmail.com>
2019-05-08 18:59:51 +0200

    Don't show middle hoshis when board size is 7
    Fix #12

--- src/helper.js
+++ src/helper.js
@@ -11,7 +11,7 @@ exports.vertexEquals = ([x1, y1], [x2, y2]) => x1 === x2 && y1 === y2
 exports.lineEquals = ([v1, w1], [v2, w2]) => exports.vertexEquals(v1, v2) && exports.vertexEquals(w1, w2)
 
 exports.getHoshis = function(width, height) {
-    if (Math.min(width, height) < 6) return []
+    if (Math.min(width, height) <= 6) return []
 
     let [nearX, nearY] = [width, height].map(x => x >= 13 ? 3 : 2)
     let [farX, farY] = [width - nearX - 1, height - nearY - 1]
@@ -19,11 +19,11 @@ exports.getHoshis = function(width, height) {
 
     let result = [[nearX, farY], [farX, nearY], [farX, farY], [nearX, nearY]]
 
-    if (width % 2 !== 0 && height % 2 !== 0)
+    if (width % 2 !== 0 && height % 2 !== 0 && width !== 7 && height !== 7)
         result.push([middleX, middleY])
-    if (width % 2 !== 0)
+    if (width % 2 !== 0 && width !== 7)
         result.push([middleX, nearY], [middleX, farY])
-    if (height % 2 !== 0)
+    if (height % 2 !== 0 && height !== 7)
         result.push([nearX, middleY], [farX, middleY])
 
     return result
