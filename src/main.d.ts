e7cd178380e686cd889c23ac276315c1947f7a26
Yichuan Shen <shen.yichuan@gmail.com>
2021-09-07 20:21:44 +0200

    Fix default exports

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,2 +1,4 @@
-export * from "./Goban";
-export * from "./BoundedGoban";
+export { default as Goban } from "./Goban";
+export { default as BoundedGoban } from "./Goban";
+export type * from "./Goban";
+export type * from "./BoundedGoban";
