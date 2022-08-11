c3fb0d74a4bc13a8e23972b54d1500ad9e95150b
Yichuan Shen <shen.yichuan@gmail.com>
2022-08-12 00:21:01 +0200

    Make ready for SVG rendering

--- src/Grid.js
+++ src/Grid.js
@@ -22,18 +22,8 @@ class Grid extends Component {
       xs.length > 0 &&
       ys.length > 0 &&
       h(
-        "svg",
-        {
-          className: "shudan-grid",
-          style: {
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            zIndex: 0,
-          },
-        },
+        "g",
+        { className: "shudan-grid" },
 
         // Draw grid lines
 
