143b9bd17b8b70fe97b22ed06f4a3838c3a9bf19
Yichuan Shen <shen.yichuan@gmail.com>
2022-08-14 14:26:37 +0200

    Better grid

--- src/Grid.js
+++ src/Grid.js
@@ -4,6 +4,7 @@ import { useMemo } from "preact/hooks";
 export default function Grid(props) {
   let { vertexSize, width, height, xs, ys, hoshis } = props;
   let halfVertexSize = vertexSize / 2;
+  let fl = Math.floor;
 
   return useMemo(
     () =>
@@ -32,8 +33,8 @@ export default function Grid(props) {
             key: `h${i}`,
 
             className: "shudan-gridline shudan-horizontal",
-            x: x,
-            y: (2 * i + 1) * halfVertexSize,
+            x: fl(x),
+            y: fl((2 * i + 1) * halfVertexSize - 0.5),
             width:
               xs[xs.length - 1] === width - 1
                 ? (2 * xs.length - 1) * halfVertexSize - x
@@ -49,8 +50,8 @@ export default function Grid(props) {
             key: `v${i}`,
 
             className: "shudan-gridline shudan-vertical",
-            x: (2 * i + 1) * halfVertexSize,
-            y: y,
+            x: fl((2 * i + 1) * halfVertexSize - 0.5),
+            y: fl(y),
             width: 1,
             height:
               ys[ys.length - 1] === height - 1
@@ -70,8 +71,8 @@ export default function Grid(props) {
             key: [x, y].join("-"),
 
             className: "shudan-hoshi",
-            cx: (2 * i + 1) * halfVertexSize + 0.5,
-            cy: (2 * j + 1) * halfVertexSize + 0.5,
+            cx: fl((2 * i + 1) * halfVertexSize - 0.5) + 0.5,
+            cy: fl((2 * j + 1) * halfVertexSize - 0.5) + 0.5,
             r: ".1em",
           });
         })
