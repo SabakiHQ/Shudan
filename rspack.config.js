dc211cf95b803e3f248e5105b1610987e3fab109
Yichuan Shen <shen.yichuan@gmail.com>
2026-03-25 17:07:12 +0100

    Add rspack

---
+++ rspack.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from "@rspack/cli";
+
+export default defineConfig({
+  entry: {
+    main: "./src/main.ts",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: [/node_modules/],
+        loader: "builtin:swc-loader",
+        options: {
+          jsc: {
+            parser: {
+              syntax: "typescript",
+              jsx: true,
+            },
+            transform: {
+              react: {
+                runtime: "automatic",
+                importSource: "sinho",
+              },
+            },
+          },
+        },
+        type: "javascript/auto",
+      },
+    ],
+  },
+  devServer: {
+    static: ["./demo"],
+  },
+});
