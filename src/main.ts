dc7989d868e16a58c2303c8a74d820c17fae6410
Yichuan Shen <shen.yichuan@gmail.com>
2026-03-28 00:40:51 +0100

    Rename stones.tsx > stone.tsx

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,5 @@ export { Vertex } from "./vertex.ts";
 export { unit, unitSvg } from "./utils.ts";
 export { Layer, type LayerOptions } from "./layers/layer.tsx";
 export { GridLayer } from "./layers/grid.tsx";
-export { StoneLayer as StonesLayer } from "./layers/stones.tsx";
+export { StoneLayer as StonesLayer } from "./layers/stone.tsx";
 export { MarkerLayer as MarkersLayer, type Marker } from "./layers/marker.tsx";
