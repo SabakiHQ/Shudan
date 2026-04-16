8c9623fa560f54e16f43e2aec791a4233d7cd193
Yichuan Shen <shen.yichuan@gmail.com>
2026-04-16 02:40:42 +0200

    Add shudan- prefix to light DOM references

--- src/utils.ts
+++ src/utils.ts
@@ -4,7 +4,7 @@ export function useLightDomReference(
   lightDomHref: Signal<string | null | undefined>,
   container: RefSignal<Element | undefined>,
 ): string {
-  const shadowDomId = "lightdom-" + crypto.randomUUID();
+  const shadowDomId = "shudan-lightdom-" + crypto.randomUUID();
 
   useEffect(() => {
     // Clone referenced light DOM elements into the shadow DOM so they can
